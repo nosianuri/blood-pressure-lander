582412957c457d4eb4b29e605c205479df553a62
nosianuri <nurinosia@gmail.com>
2023-03-05 22:18:05 +0600

    add alpilean

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
+import Alpilean from './Pages/Alpilean/Alpilean';
 
 function App() {
   return (
     <div>
-    <Routes>
-    <Route path='/bloodpressure911' element={<Home />} />
-    </Routes>
+      <Routes>
+        <Route path='/bloodpressure911' element={<Home />} />
+        <Route path='/alpilean' element={<Alpilean />} />
+      </Routes>
     </div>
   );
 }
