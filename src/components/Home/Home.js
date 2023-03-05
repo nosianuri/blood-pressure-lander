d9a7dc77060c1b1d4a6b1b5dc3e4bf72ee1215bf
nosianuri <nurinosia@gmail.com>
2023-03-05 23:20:35 +0600

    add helmet

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import Mainbody from '../Mainbody/Mainbody'
 
 const Home = () => {
   return (
     <div>
-        <Mainbody />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Blood Pressure 911</title>
+      </Helmet>
+      <Mainbody />
     </div>
   )
 }
