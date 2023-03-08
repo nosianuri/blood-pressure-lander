611f370f1de15741cd9e49b2c8c66ea5b7be3562
nosianuri <nurinosia@gmail.com>
2023-03-09 00:24:52 +0600

    update alpilean page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import Alpilean from './Pages/Alpilean/Alpilean';
+import AlpileanHome from './Pages/Alpilean/AlpileanHome';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/bloodpressure911' element={<Home />} />
-        <Route path='/alpilean' element={<Alpilean />} />
+        <Route path='/alpilean' element={<AlpileanHome />} />
       </Routes>
     </div>
   );
