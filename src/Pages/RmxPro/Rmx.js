da0cc12396fa741064418c48affb6aeb01433e8a
nosianuri <nurinosia@gmail.com>
2023-03-27 21:53:18 +0600

    update text

--- src/Pages/RmxPro/Rmx.js
+++ src/Pages/RmxPro/Rmx.js
@@ -14,11 +14,11 @@ const Rmx = () => {
             </Helmet>
             <div className='bg-white alpine'>
                 <div className='max-w-7xl mx-auto justify-center px-4 py-10'>
-                    <div className='text-center font-bold text-4xl text-[#fff] bg-purple-500 sm:w-[900px] mx-auto'>
+                    {/* <div className='text-center font-bold text-4xl text-[#fff] bg-purple-500 sm:w-[900px] mx-auto'>
                         <a href="/">08060380375</a> or
                         <a href="/"> 08078440640</a> or
                         <a href="/"> 08121652243</a>
-                    </div>
+                    </div> */}
                     {/* <div>
                         <input
                             className="border border-gray-400 p-2 rounded-lg w-full"
@@ -27,7 +27,7 @@ const Rmx = () => {
                             onChange={(e) => setText(e.target.value)}
                         />
                     </div> */}
-                    <div className='text-6xl  p-1 text-center my-5 !leading-normal'>
+                    <div className='sm:text-6xl text-3xl  p-1 text-center my-5 !leading-normal'>
                         <span className='text-red-700 font-bold'>Important Warning!</span> Don't Use The Products If Your Partner Can't Cope With Over 50 Minutes of Action.
                     </div>
                     <p className='text-center text-2xl !leading-normal'>The products you're about to see will help you cure <span className='font-bold underline'>Premature Eja****tion, weak manhood, quick explusion, weakness after explusion, low urge, tiredness during "action" and back pain</span>. They are 100% effective and approved by NAFDAC. <span className='text-orange-500 italic'>Kindly click the link below to see the Products and also see testimonials from past users below.</span></p>
@@ -41,7 +41,7 @@ const Rmx = () => {
                         <li>Lack of stamina.</li>
                     </ul>
                     <p className='text-red-800 font-bold text-center text-2xl'>By clicking the link below, you guarantee that you're above the age of 18 and you will not abuse the usage of the products.</p>
-                    <div className='text-center text-4xl my-8 font-bold'><a href="/" className='text-white bg-purple-700 underline px-4'>Click Here To See The Herbal Products</a></div>
+                    <div className='text-center sm:text-4xl text-lg my-8 font-bold'><a href="/" className='text-white bg-purple-700 underline px-4'>Click here to See The product</a></div>
                 </div>
             </div>
         </>
