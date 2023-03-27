fa806f203b275b39a9733829460669fc1d186d7e
nosianuri <nurinosia@gmail.com>
2023-03-27 22:15:31 +0600

    change bg

--- src/Pages/RmxPro/Rmx.js
+++ src/Pages/RmxPro/Rmx.js
@@ -27,12 +27,12 @@ const Rmx = () => {
                             onChange={(e) => setText(e.target.value)}
                         />
                     </div> */}
-                    <div className='sm:text-6xl text-3xl  p-1 text-center my-5 !leading-normal'>
-                        <span className='text-red-700 font-bold'>Important Warning!</span> Don't Use The Products If Your Partner Can't Cope With Over 50 Minutes of Action.
+                    <div className='sm:text-6xl text-3xl  p-1 text-center mb-5 !leading-normal'>
+                        <span className='text-red-700 font-bold'>Important Warning!</span> <br /><span className=' bg-yellow-300 px-1'>Don't Use The Product If Your Partner Can't Cope With Over 50 Minutes of Action.</span> 
                     </div>
-                    <p className='text-center text-2xl !leading-normal'>The products you're about to see will help you cure <span className='font-bold underline'>Premature Eja****tion, weak manhood, quick explusion, weakness after explusion, low urge, tiredness during "action" and back pain</span>. They are 100% effective and approved by NAFDAC. <span className='text-orange-500 italic'>Kindly click the link below to see the Products and also see testimonials from past users below.</span></p>
-                    <p className='text-blue-700 text-center font-bold my-5 text-2xl'>Click here to see testimonials From Users</p>
-                    <p className='text-xl'>Highlighted below are some of the problems we've helped people to solve with our powerful herbal products:</p>
+                    <p className='text-center text-2xl !leading-normal'>The product you're about to see will help you cure <span className='font-bold underline'>Premature Eja****tion, weak manhood, quick explusion, weakness after explusion, low urge, tiredness during "action" and back pain</span>. They are 100% effective and approved by NAFDAC. <span className='text-orange-500 italic'>Kindly click the link below to see the Product and also see testimonials from past users below.</span></p>
+                    {/* <p className='text-blue-700 text-center font-bold my-5 text-2xl'>Click here to see testimonials From Users</p> */}
+                    <p className='text-xl mt-5'>Highlighted below are some of the problems we've helped people to solve with our powerful herbal product:</p>
                     <ul className='ul-article text-xl'>
                         <li>Not able to last more than 2 minutes.</li>
                         <li>No urge to make love at all.</li>
@@ -40,8 +40,12 @@ const Rmx = () => {
                         <li>Tiredness after 1 round or within 5 minutes of action.</li>
                         <li>Lack of stamina.</li>
                     </ul>
-                    <p className='text-red-800 font-bold text-center text-2xl'>By clicking the link below, you guarantee that you're above the age of 18 and you will not abuse the usage of the products.</p>
+                    <p className='text-red-800 font-bold text-center text-2xl'>By clicking the link below, you guarantee that you're above the age of 18 and you will not abuse the usage of the product.</p>
+                    <a href="https://www.r3hlvtrk.com/3J67C/8C1LQ73/">
+                        <img src="" alt="" />
+                    </a>
                     <div className='text-center sm:text-4xl text-lg my-8 font-bold'><a href="/" className='text-white bg-purple-700 underline px-4'>Click here to See The product</a></div>
+                    {/* <div>Disclaimer: Privacy Policy </div> */}
                 </div>
             </div>
         </>
