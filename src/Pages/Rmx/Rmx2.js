ed3e63d9c1d2a109493c74bc2874761774210b4b
nosianuri <nurinosia@gmail.com>
2023-03-30 00:37:08 +0600

    add happy image

--- src/Pages/Rmx/Rmx2.js
+++ src/Pages/Rmx/Rmx2.js
@@ -13,15 +13,21 @@ const Rmx2 = () => {
             </Helmet>
             <div className='bg-white alpine'>
                 <div className='max-w-[1150px] mx-auto justify-center px-4 py-10'>
-                <h1 className='text-red-700 font-bold sm:text-6xl text-3xl text-center mb-5'>Important Warning!</h1>
-                    <p className='max-w-[800px] mx-auto sm:text-4xl text-2xl  text-center mb-5 !leading-normal '>
-                         <span className='bg-yellow-300 px-1 '>Don't Use The Product If Your Partner Can't Cope With Over 50 Minutes of Action.</span>
+                    <h1 className='text-red-700 font-bold sm:text-6xl text-3xl text-center mb-5'>Important Warning!</h1>
+                    <p className='max-w-[800px] mx-auto sm:text-4xl text-xl  text-center sm:mb-10 mb-5 !leading-normal '>
+                        <span className='bg-yellow-300 px-1 '>Don't Use The Product If Your Partner Can't Cope With Over 50 Minutes of Action.</span>
                     </p>
-                    
+
                     <h5 className='sm:text-4xl text-2xl mb-5 font-bold !leading-normal indent-8'>✨Before Seeing the Product Read the Success Story from one of our <span className=' million'>Happiest Man!</span>✨</h5>
                     <p className='text-red-500 mb-5 font-bold'>[Insert a Copyright Free Single Male Happy Image Here]</p>
-                    <p className='mb-2 text-lg font-bold indent-8'>34-year-old Paolo became famous because in just over ten days his pen*is increased in size from 13 cm to ... 23 cm. According to the owner of one of the most magnificent phalluses in the world, he never expected his method to have such an effect.</p>
-                    <p className='mb-5 indent-8'>It happened. I am experiencing the best sex of my life. I gallop it once, two, three and then I stop counting. I finally got rid of the small pen*is complexes and regained my self-confidence.</p>
+                    <div className='sm:flex items-center gap-5'>
+                        <div><img src="https://i.ibb.co/DGF2pyZ/caple-goal.png" alt="" className='w-96' /></div>
+                        <div>
+                            <p className='mb-2 text-lg font-bold indent-8'>34-year-old Paolo became famous because in just over ten days his pen*is increased in size from 13 cm to ... 23 cm. According to the owner of one of the most magnificent phalluses in the world, he never expected his method to have such an effect.</p>
+                            <p className='mb-5 indent-8'>It happened. I am experiencing the best sex of my life. I gallop it once, two, three and then I stop counting. I finally got rid of the small pen*is complexes and regained my self-confidence.</p>
+                        </div>
+                    </div>
+
                     <h5 className='font-bold my-5 border-2 border-white border-l-orange-500 px-5 sm:text-3xl text-xl !leading-normal text-orange-500'>A few months ago I was a shy man with an embarrassing problem, which is…</h5>
                     <p className='mb-5 indent-8'>The micro pen*is. A problem I don't wish on anyone. The thought of having such a small thing between my legs didn't let me sleep at night. In addition, the whole emotional and working sphere was going very badly. And if that wasn't enough I also had erection problems.</p>
                     <h5 className='font-bold my-5 border-2 border-white border-l-orange-500 px-5 sm:text-3xl text-xl !leading-normal text-orange-500'>It tells us that for women size doesn't matter but what really matters is character. BULLSHIT.</h5>
@@ -33,25 +39,25 @@ const Rmx2 = () => {
                     <p className='mb-5 indent-8'>It was an accident. When I came out of this "dark period", I started looking for solutions to increase the size of my pen*is. I have also considered methods like surgical pen*is enlargement, but it costs a bit too much and secondly it is associated with health risk. I soon discovered a natural method that lengthens the pen*is and increases sexual abilities at the same time.
                         <br /> <br />
                         What I'm talking about I found eh .. on adult sites. I couldn't get over how those guys could have such big poles between their legs. On one of these pages there was an article dedicated to natural methods to enlarge the pen*is and so I came across <a href="https://www.r3hlvtrk.com/3J67C/8C1LQ73/"
-                        className='font-bold underline hover:no-underline text-orange-500'>Rhino Max</a> . I immediately bought 2 packs - I preferred to overdo it. And with great joy I discovered that the effects were visible not even having finished the first bottle. They were shocking </p>
+                            className='font-bold underline hover:no-underline text-orange-500'>Rhino Max</a> . I immediately bought 2 packs - I preferred to overdo it. And with great joy I discovered that the effects were visible not even having finished the first bottle. They were shocking </p>
                     <h5 className='font-bold my-5 border-2 border-white border-l-orange-500 px-5 sm:text-3xl text-xl !leading-normal text-orange-500'>I'll start with the fact of the erection</h5>
                     <p className='mb-5 indent-8'>As I mentioned, I had the problem with this as well. I think it was also due to the stress and awareness of having a small pen*is. When I first took <a href="https://www.r3hlvtrk.com/3J67C/8C1LQ73/"
                         className='font-bold underline hover:no-underline text-orange-500'>Rhino Max</a>, something amazing happened - my pen*is became hard when erect and stayed upright. It never happened. In the next few days I was able to observe how both the circumference and the length increased rapidly. Now I know that my body has developed smooth muscle blood vessels, which has increased blood flow into the pen*is and, consequently, its size. But what happened after 2 weeks exceeded my expectations.
                         <br /><br />
                         <div className=' rounded-lg border-4 border-gray-400 px-4 py-2'>
-                        I thought I'd see how my little (ie not so little anymore) will react in a "natural" environment. I made an appointment with a girl. Specifically with my ex. I will never forget the look she gave my package once we arrived at her apartment. Her eyes said it all: shock, disbelief but also admiration. When I penetrated her I heard a scream. A scream of pleasure to be clear. We had sex 5 times that night. I was surprised that I didn't need to take long breaks between acts. It had never happened before that moment. But most importantly, my micro pen*is now looked more like a steel rod.
-                        <br /> <br />
-                        It has been more than six months since then and I continue to experience the best sex of my life. My ex wanted to get
-                        back to me, but now I've found a girl who fulfills all my erotic fantasies.
-                        <br /> <br />
-                        Finishing this somewhat long speech, I wanted to tell you one last thing: don't wonder within yourselves but look for a solution. Look for safe and certified solutions. I advise against surgery - too much risk of complications.
+                            I thought I'd see how my little (ie not so little anymore) will react in a "natural" environment. I made an appointment with a girl. Specifically with my ex. I will never forget the look she gave my package once we arrived at her apartment. Her eyes said it all: shock, disbelief but also admiration. When I penetrated her I heard a scream. A scream of pleasure to be clear. We had sex 5 times that night. I was surprised that I didn't need to take long breaks between acts. It had never happened before that moment. But most importantly, my micro pen*is now looked more like a steel rod.
+                            <br /> <br />
+                            It has been more than six months since then and I continue to experience the best sex of my life. My ex wanted to get
+                            back to me, but now I've found a girl who fulfills all my erotic fantasies.
+                            <br /> <br />
+                            Finishing this somewhat long speech, I wanted to tell you one last thing: don't wonder within yourselves but look for a solution. Look for safe and certified solutions. I advise against surgery - too much risk of complications.
                         </div>
                         <br />
                         I highly recommend <a href="https://www.r3hlvtrk.com/3J67C/8C1LQ73/"
-                        className='font-bold underline hover:no-underline text-orange-500'>Rhino Max</a> . This product has helped me tremendously. I can say it changed my life. Of course it can act differently from person to person, but I'm sure you will be able to increase your pen*is by a minimum of 4-5 cm.
+                            className='font-bold underline hover:no-underline text-orange-500'>Rhino Max</a> . This product has helped me tremendously. I can say it changed my life. Of course it can act differently from person to person, but I'm sure you will be able to increase your pen*is by a minimum of 4-5 cm.
                         <br /> <br />
                         Below is the link where you can find more information about <a href="https://www.r3hlvtrk.com/3J67C/8C1LQ73/"
-                        className='font-bold underline hover:no-underline text-orange-500'>Rhino Max</a>. Good luck gentlemen!
+                            className='font-bold underline hover:no-underline text-orange-500'>Rhino Max</a>. Good luck gentlemen!
                     </p>
                     <img src="https://i.ibb.co/551ZX6b/rmx-benefit.png" alt="" className='mx-auto my-5 w-3/4' />
                     <p className='text-center text-2xl !leading-normal'>The product you're about to see will help you cure <span className='font-bold underline'>Premature Eja****tion, weak manhood, quick explusion, weakness after explusion, low urge, tiredness during "action" and back pain</span>. They are 100% effective and approved by NAFDAC. </p>
