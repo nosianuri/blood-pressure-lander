20a6013332a27e5134e8a003a58809de02bc1840
nosianuri <nurinosia@gmail.com>
2023-03-30 23:27:50 +0600

    update life line link

--- src/Pages/LifelineSg/LifelineSg.js
+++ src/Pages/LifelineSg/LifelineSg.js
@@ -13,14 +13,14 @@ const LifelineSg = () => {
             <div>
                 <div className='  h-screen'>
                     <div className='max-w-[950px] mx-auto justify-center  alpine shadow-lg '>
-                    <a href="http://llsa.go2cloud.org/aff_c?offer_id=775&aff_id=1570">
+                    <a href="https://www.r3hlvtrk.com/3J67C/8FKSNGM/">
                     <div className='bg-[#145fab] h-[50px] mb-5'></div>
                     <img src="https://i.ibb.co/HG4BQJC/llc-logo.webp" alt="" className='mx-auto' />
                     <div className='px-2'>
                     <h1 className='font-bold mt-5 sm:text-5xl text-2xl !leading-normal text-center text-red-800'>Knowledge is Power 💪</h1>
                     <h1 className='max-w-[850px] mx-auto font-bold mb-5 sm:text-5xl text-2xl !leading-normal text-center text-[#033F6C]'> <span className='text-green-800'>Lifeline Screening</span> Gives You the Tools You Need</h1>
                     <p className='max-w-[850px] mx-auto sm:mb-10 mb-10 text-[#033F6C]'><span className='wnever'>W</span>hen it comes to your health, knowledge is power. Lifeline Screening can give you the tools you need to take control of your health and well-being. Our screening tests are quick, easy, and non-invasive, and our experienced healthcare professionals will provide you with accurate and reliable results. Whether you want to learn about your risk for heart disease, stroke, or other conditions, Lifeline Screening has you covered. Take the first step towards a healthier you – schedule your appointment today. Protect Your Health with Lifeline Screening.</p>
-                    <a className='' href="http://llsa.go2cloud.org/aff_c?offer_id=775&aff_id=1570">
+                    <a className='' href="https://www.r3hlvtrk.com/3J67C/8FKSNGM/">
                         <img src={lifegiphy} alt="" className='sm:w-2/3 mx-auto' />
                     </a>
                     </div>
